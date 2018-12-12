14a58426de91b730de0fa4429bdfead06e65e845
Brendan Whiting <brendan.whiting@cognizant.com>
2018-12-11 23:10:16 -0500

    date working

--- src/app/components/todo-input/todo-input.component.ts
+++ src/app/components/todo-input/todo-input.component.ts
@@ -8,20 +8,21 @@ import { TodoService } from '../../services/todo.service';
   styleUrls: ['./todo-input.component.css']
 })
 export class TodoInputComponent implements OnInit {
-   
   public title: string;
   public description: string;
-  public time  = new Date;
+  public time: Date;
 
   constructor(private todoService: TodoService) {
     this.title  = '';
     this.description  = '';
+    this.time = new Date();
   }
 
   ngOnInit() {
   }
 
-  private addTodo(): void {
-    this.todoService.addTodo(this.title,this.description, this.time);
+  public addTodo(): void {
+    console.log(this.time);
+    this.todoService.addTodo(this.title, this.description, new Date(this.time));
   }
 }
